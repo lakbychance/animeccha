f40624440ed6216a22c40b9105672f2b5ab6414f
Lakshya Thakur <lapstjup@gmail.com>
2020-12-15 16:19:43 +0530

    Add placeholder div for anime card

--- src/Anime/AnimeCard/AnimeCard.tsx
+++ src/Anime/AnimeCard/AnimeCard.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import styles from "./AnimeCard.module.css";
+
 const AnimeCard = ({ title, thumbnailUrl }: any) => {
+  const [showImage, setShowImage] = useState(false);
+  useEffect(() => {
+    const image = new Image();
+    image.onload = () => {
+      setShowImage(true);
+    };
+    image.src = thumbnailUrl;
+  }, [thumbnailUrl]);
   const animeCardVariant = {
     initial: {
       opacity: 0,
-      y:'-100vh'
+      y: "-100vh",
     },
     final: {
       opacity: 1,
-      y:0
+      y: 0,
     },
   };
   return (
@@ -20,7 +29,22 @@ const AnimeCard = ({ title, thumbnailUrl }: any) => {
       className={styles.animeCard}
     >
       <div className={styles.animeThumbnail}>
-        <img alt={title} src={thumbnailUrl}></img>
+        {showImage ? (
+          <img
+            alt={title}
+            className={styles.imageContainer}
+            src={thumbnailUrl}
+          ></img>
+        ) : (
+          <div className={styles.placeHolderContainer}>
+            <motion.div
+              animate={{ x: [-200, 200] }}
+              transition={{ yoyo: Infinity }}
+              className={styles.shimmer}
+            ></motion.div>
+            <span>{title}</span>
+          </div>
+        )}
       </div>
     </motion.div>
   );
