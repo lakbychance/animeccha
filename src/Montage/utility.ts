b80b08e69886e0fffd18f35e8587ad7743e53437
Lakshya Thakur <lapstjup@gmail.com>
2020-12-15 17:44:32 +0530

    apply cap to transformation paramters

--- src/Montage/utility.ts
+++ src/Montage/utility.ts
@@ -24,7 +24,7 @@ const coverImg = (context:any, img:HTMLImageElement, type:string) => {
   };
 
 const currentFrame = (path:string,index:number) =>
-  `/${path}/${index.toString().padStart(4, "0")}.jpg?nf_resize=fit&w=${window.innerWidth}`; 
+  `/${path}/${index.toString().padStart(4, "0")}.jpg?nf_resize=fit&w=${Math.min(window.innerWidth,800)}`; 
 
 const preloadImages = (path:string,initial:number,frameCount:number) => {
   const imagePromisesArray:any = [];
