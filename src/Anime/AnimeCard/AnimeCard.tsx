cd787cd2ca0c7a0c7bd4879f54b69f6b5ba4bc99
Lakshya Thakur <lapstjup@gmail.com>
2020-12-19 19:42:21 +0530

    Try smooth image loading

--- src/Anime/AnimeCard/AnimeCard.tsx
+++ src/Anime/AnimeCard/AnimeCard.tsx
@@ -1,16 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import styles from "./AnimeCard.module.css";
 
 const AnimeCard = ({ title, thumbnailUrl }: any) => {
-  const [image, setImage] = useState<HTMLImageElement>();
-  useEffect(() => {
-    const image = new Image();
-    image.onload = () => {
-      setImage(image);
-    };
-    image.src = thumbnailUrl;
-  }, [thumbnailUrl]);
+  const [showImage, setShowImage] = useState(false);
+
   const animeCardVariant = {
     initial: {
       opacity: 0,
@@ -29,17 +23,20 @@ const AnimeCard = ({ title, thumbnailUrl }: any) => {
       className={styles.animeCard}
     >
       <div className={styles.animeThumbnail}>
-        {image ? (
-          <img
-            alt={title}
-            className={styles.imageContainer}
-            src={thumbnailUrl}
-          ></img>
-        ) : (
+        <img
+          alt={title}
+          className={`${styles.imageContainer} ${
+            showImage ? styles.visible : styles.hidden
+          }`}
+          src={thumbnailUrl}
+          onLoad={() => setShowImage(true)}
+        ></img>
+
+        {!showImage && (
           <div className={styles.placeHolderContainer}>
             <motion.div
-              animate={{ x: [-500,500]}}
-              transition={{ duration:'1',repeat:Infinity}}
+              animate={{ x: [-500, 500] }}
+              transition={{ duration: "1", repeat: Infinity }}
               className={styles.shimmer}
             ></motion.div>
             <span>{title}</span>
