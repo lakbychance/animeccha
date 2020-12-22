186aea323b36d06308f614799efc97f6d90b6eb3
Lakshya Thakur <lapstjup@gmail.com>
2020-12-22 18:18:21 +0530

    Scroll to top on pathname change

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,16 @@
-import React, { CSSProperties } from "react";
+import React, { CSSProperties, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import styles from "./Layout.module.css";
 
 interface ComponentProps {
   layoutStyles?: CSSProperties;
 }
 const Layout: React.FC<ComponentProps> = ({ children, layoutStyles }) => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className={styles.layout} style={{ ...layoutStyles }}>
       {children}
